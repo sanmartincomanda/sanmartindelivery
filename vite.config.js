52957ae686e9c2e74d3a0451abe65573a30c3369
sanmartincomanda <carnessanmartingranada@gmail.com>
2025-06-18 12:13:36 -0600

    Actualizacion ultima

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true // hace que Vite escuche en 0.0.0.0 y sea accesible desde otras máquinas
-  }
-})
+  build: {
+    outDir: 'dist',
+  },
+});
